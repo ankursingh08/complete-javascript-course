644094fdcbf406dddede2bf9714004577411c956
Ankur Singh <ankur.singh4012@gmail.com>
2022-05-26 21:01:52 +0200

    my practice branch

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function fruitJuice(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const juice = fruitJuice(5, 8);
+console.log(juice);
